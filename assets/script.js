725e3c30ef58e9722d33f7e8ce47fccafae08fde
Ryan Dowdy <rdowdy@instructors.2u.com>
2022-09-29 12:01:44 -0400

    Added second question. Refactored question rendering function.

--- assets/script.js
+++ assets/script.js
@@ -12,20 +12,46 @@ var questions = [
             "Dancing Orangutans Movie"
         ],
         correctAnswer: "Document Object Model"
+    },
+    {
+        question: "What does CSS stand for?",
+        possibleAnswers: [
+            "Cool Skateboarding Salamanders",
+            "Cascading Style Sheets",
+            "Culinary Salmon State",
+            "Coding Standards & Solidness"
+        ],
+        correctAnswer: "Cascading Style Sheets"
     }
 ];
 
 startQuizBtn.addEventListener("click", startQuiz);
 
 function startQuiz() {
-    var currentQuestion = questions[0];
-    quizQuestionEl.textContent = currentQuestion.question;
+    moveToQuestion(0);
+    startQuizBtn.setAttribute("style", "display: none;");
+}
 
+function moveToQuestion(nextQuestionIndex) {
+    // get the next question from the questions array
+    var currentQuestion = questions[nextQuestionIndex];
+    // render the question
+    quizQuestionEl.textContent = currentQuestion.question;
+    // render the possible answers
     for (var i = 0; i < currentQuestion.possibleAnswers.length; i++) {
         var li = document.createElement("li");
         li.textContent = currentQuestion.possibleAnswers[i];
+
+        li.addEventListener("click", function(event) {
+            if (event.target.textContent === currentQuestion.correctAnswer) {
+                console.log("Correct Answer");
+            } else {
+                console.log("Wrong Answer");
+            }
+
+            moveToQuestion(nextQuestionIndex + 1);
+        });
+
         possibleAnswersEl.appendChild(li);
     }
-
-    startQuizBtn.setAttribute("style", "display: none;");
 }
